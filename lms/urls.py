93f68dc1f63ccc25ae817b3588621bf490f8ccf6
longnp030 <philongg2000@gmail.com>
2020-12-21 20:22:17 +0700

    21/12 some changes, cearch bar, ...

--- lms/urls.py
+++ lms/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView
-from django.views.generic import TemplateView
 
 from . import views
 
