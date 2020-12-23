9f9de715315afdaa795f6b86d36704e359c916d0
longnp030 <philongg2000@gmail.com>
2020-12-24 01:10:49 +0700

    final changes 23/12

--- eLearning/settings.py
+++ eLearning/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'eLearning.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moc',
+        'NAME': 'lms',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
